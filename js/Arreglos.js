79ede75782c247fde4f56510a78734e9c6868e99
Jeison Rodriguez <jeison.rodriguez@gmail.com>
2022-10-05 15:38:59 -0300

    arreglo de imagenes llamada desde DOM

---
+++ js/Arreglos.js
@@ -0,0 +1,346 @@
+// Arreglo de imagenes 
+let calle1 = document.getElementById('calle1');
+let calle2 = document.getElementById('calle2');
+let calle3 = document.getElementById('calle3');
+let calle4 = document.getElementById('calle4');
+let calle5 = document.getElementById('calle5');
+let calle6 = document.getElementById('calle6');
+let calle7 = document.getElementById('calle7');
+let calle8 = document.getElementById('calle8');
+let calle9 = document.getElementById('calle9');
+let calle10 = document.getElementById('calle10');
+let calle11 = document.getElementById('calle11');
+let calle12 = document.getElementById('calle12');
+let calle13 = document.getElementById('calle13');
+let calle14 = document.getElementById('calle14');
+let calle15 = document.getElementById('calle15');
+let calle16 = document.getElementById('calle16');
+let calle17 = document.getElementById('calle17');
+let calle18 = document.getElementById('calle18');
+let calle19 = document.getElementById('calle19');
+let calle20 = document.getElementById('calle20');
+let calle21 = document.getElementById('calle21');
+let calle22 = document.getElementById('calle22');
+let calle23 = document.getElementById('calle23');
+let calle24 = document.getElementById('calle24');
+let calle25 = document.getElementById('calle25');
+let calle26 = document.getElementById('calle26');
+let calle27 = document.getElementById('calle27');
+let calle28 = document.getElementById('calle28');
+let calle29 = document.getElementById('calle29');
+let calle30 = document.getElementById('calle30');
+let calle31 = document.getElementById('calle31');
+let calle32 = document.getElementById('calle32');
+let calle33 = document.getElementById('calle33');
+let calle34 = document.getElementById('calle34');
+let calle35 = document.getElementById('calle35');
+let calle36 = document.getElementById('calle36');
+let calle37 = document.getElementById('calle37');
+
+function mostrarCalle (){
+     calle1.src = "img/Para Jeison/En la calle/c1.jpeg";
+     calle2.src = "img/Para Jeison/En la calle/c2.jpeg";
+     calle3.src = "img/Para Jeison/En la calle/c3.jpeg";
+     calle4.src = "img/Para Jeison/En la calle/c4.jpeg";
+     calle5.src = "img/Para Jeison/En la calle/c5.jpeg";
+     calle6.src = "img/Para Jeison/En la calle/c6.jpeg";
+     calle7.src = "img/Para Jeison/En la calle/c7.jpeg";
+     calle8.src = "img/Para Jeison/En la calle/c8.jpeg";
+     calle9.src = "img/Para Jeison/En la calle/c9.jpeg";
+     calle10.src = "img/Para Jeison/En la calle/c10.jpeg";
+     calle11.src = "img/Para Jeison/En la calle/c11.jpeg";
+     calle12.src = "img/Para Jeison/En la calle/c12.jpeg";
+     calle13.src = "img/Para Jeison/En la calle/c13.jpeg";
+     calle14.src = "img/Para Jeison/En la calle/c14.jpeg";
+     calle15.src = "img/Para Jeison/En la calle/c15.jpeg";
+     calle16.src = "img/Para Jeison/En la calle/c16.jpeg";
+     calle17.src = "img/Para Jeison/En la calle/c17.jpeg";
+     calle18.src = "img/Para Jeison/En la calle/c18.jpeg";
+     calle19.src = "img/Para Jeison/En la calle/c19.jpeg";
+     calle20.src = "img/Para Jeison/En la calle/c20.jpeg";
+     calle21.src = "img/Para Jeison/En la calle/c21.jpeg";
+     calle22.src = "img/Para Jeison/En la calle/c22.jpeg";
+     calle23.src = "img/Para Jeison/En la calle/c23.jpeg";
+     calle24.src = "img/Para Jeison/En la calle/c24.jpeg";
+     calle25.src = "img/Para Jeison/En la calle/c25.jpeg";
+     calle26.src = "img/Para Jeison/En la calle/c26.jpeg";
+     calle27.src = "img/Para Jeison/En la calle/c27.jpeg";
+     calle28.src = "img/Para Jeison/En la calle/c28.jpeg";
+     calle29.src = "img/Para Jeison/En la calle/c29.jpeg";
+     calle30.src = "img/Para Jeison/En la calle/c30.jpeg";
+     calle31.src = "img/Para Jeison/En la calle/c31.jpeg";
+     calle32.src = "img/Para Jeison/En la calle/c32.jpeg";
+     calle33.src = "img/Para Jeison/En la calle/c33.jpeg";
+     calle34.src = "img/Para Jeison/En la calle/c34.jpeg";
+     calle35.src = "img/Para Jeison/En la calle/c35.jpeg";
+     calle36.src = "img/Para Jeison/En la calle/c36.jpeg";
+     calle37.src = "img/Para Jeison/En la calle/c37.jpeg";
+}
+
+mostrarCalle ();
+
+
+let byn1 = document.getElementById('byn1');
+let byn2 = document.getElementById('byn2');
+let byn3 = document.getElementById('byn3');
+let byn4 = document.getElementById('byn4');
+let byn5 = document.getElementById('byn5');
+let byn6 = document.getElementById('byn6');
+let byn7 = document.getElementById('byn7');
+let byn8 = document.getElementById('byn8');
+let byn9 = document.getElementById('byn9');
+let byn10 = document.getElementById('byn10');
+let byn11 = document.getElementById('byn11');
+let byn12 = document.getElementById('byn12');
+let byn13 = document.getElementById('byn13');
+let byn14 = document.getElementById('byn14');
+let byn15 = document.getElementById('byn15');
+let byn16 = document.getElementById('byn16');
+let byn17 = document.getElementById('byn17');
+let byn18 = document.getElementById('byn18');
+let byn19 = document.getElementById('byn19');
+let byn20 = document.getElementById('byn20');
+let byn21 = document.getElementById('byn21');
+let byn22 = document.getElementById('byn22');
+let byn23 = document.getElementById('byn23');
+let byn24 = document.getElementById('byn24');
+let byn25 = document.getElementById('byn25');
+let byn26 = document.getElementById('byn26');
+let byn27 = document.getElementById('byn27');
+
+function mostrarByN(){
+    byn1.src = "img/Para Jeison/Blanco y negro/b1.jpg";
+    byn2.src = "img/Para Jeison/Blanco y negro/b2.jpg";
+    byn3.src = "img/Para Jeison/Blanco y negro/b3.jpg";
+    byn4.src = "img/Para Jeison/Blanco y negro/b4.jpg";
+    byn5.src = "img/Para Jeison/Blanco y negro/b5.jpg";
+    byn6.src = "img/Para Jeison/Blanco y negro/b6.jpg";
+    byn7.src = "img/Para Jeison/Blanco y negro/b7.jpg";
+    byn8.src = "img/Para Jeison/Blanco y negro/b8.jpg";
+    byn9.src = "img/Para Jeison/Blanco y negro/b9.jpg";
+    byn10.src = "img/Para Jeison/Blanco y negro/b10.jpg";
+    byn11.src = "img/Para Jeison/Blanco y negro/b11.jpg";
+    byn12.src = "img/Para Jeison/Blanco y negro/b12.jpg";
+    byn13.src = "img/Para Jeison/Blanco y negro/b13.jpg";
+    byn14.src = "img/Para Jeison/Blanco y negro/b14.jpg";
+    byn15.src = "img/Para Jeison/Blanco y negro/b15.jpg";
+    byn16.src = "img/Para Jeison/Blanco y negro/b16.jpg";
+    byn17.src = "img/Para Jeison/Blanco y negro/b17.jpg";
+    byn18.src = "img/Para Jeison/Blanco y negro/b18.jpg";
+    byn19.src = "img/Para Jeison/Blanco y negro/b19.jpg";
+    byn20.src = "img/Para Jeison/Blanco y negro/b20.jpg";
+    byn21.src = "img/Para Jeison/Blanco y negro/b21.jpeg";
+    byn22.src = "img/Para Jeison/Blanco y negro/b22.jpeg";
+    byn23.src = "img/Para Jeison/Blanco y negro/b23.jpeg";
+    byn24.src = "img/Para Jeison/Blanco y negro/b24.jpeg";
+    byn25.src = "img/Para Jeison/Blanco y negro/b25.jpeg";
+    byn26.src = "img/Para Jeison/Blanco y negro/b26.jpeg";
+    byn27.src = "img/Para Jeison/Blanco y negro/b27.jpeg";
+   
+}
+
+mostrarByN();
+
+let flora1 = document.getElementById('flora1');
+let flora2 = document.getElementById('flora2');
+let flora3 = document.getElementById('flora3');
+let flora4 = document.getElementById('flora4');
+let flora5 = document.getElementById('flora5');
+let flora6 = document.getElementById('flora6');
+let flora7 = document.getElementById('flora7');
+let flora8 = document.getElementById('flora8');
+let flora9 = document.getElementById('flora9');
+let flora10 = document.getElementById('flora10');
+let flora11 = document.getElementById('flora11');
+let flora12 = document.getElementById('flora12');
+let flora13 = document.getElementById('flora13');
+let flora14 = document.getElementById('flora14');
+let flora15 = document.getElementById('flora15');
+
+function mostrarFlora (){
+    flora1.src = "img/Para Jeison/Flora y Fauna/f1.jpg";
+    flora2.src = "img/Para Jeison/Flora y Fauna/f2.jpeg";
+    flora3.src = "img/Para Jeison/Flora y Fauna/f3.jpg";
+    flora4.src = "img/Para Jeison/Flora y Fauna/f4.jpg";
+    flora5.src = "img/Para Jeison/Flora y Fauna/f5.jpg";
+    flora6.src = "img/Para Jeison/Flora y Fauna/f6.jpeg";
+    flora7.src = "img/Para Jeison/Flora y Fauna/f7.jpeg";
+    flora8.src = "img/Para Jeison/Flora y Fauna/f8.jpeg";
+    flora9.src = "img/Para Jeison/Flora y Fauna/f9.jpeg";
+    flora10.src = "img/Para Jeison/Flora y Fauna/f10.jpeg";
+    flora11.src = "img/Para Jeison/Flora y Fauna/f11.jpeg";
+    flora12.src = "img/Para Jeison/Flora y Fauna/f12.jpeg";
+    flora13.src = "img/Para Jeison/Flora y Fauna/f13.jpeg";
+    flora14.src = "img/Para Jeison/Flora y Fauna/f14.jpeg";
+    flora15.src = "img/Para Jeison/Flora y Fauna/f15.jpeg";
+}
+mostrarFlora ();
+
+let producto1 = document.getElementById('producto1');
+let producto2 = document.getElementById('producto2');
+let producto3 = document.getElementById('producto3');
+let producto4 = document.getElementById('producto4');
+let producto5 = document.getElementById('producto5');
+let producto6 = document.getElementById('producto6');
+
+
+function mostrarProductos(){
+
+    producto1.src = "img/Para Jeison/Fotoproducto/p1.png";
+    producto2.src = "img/Para Jeison/Fotoproducto/p2.png";
+    producto3.src = "img/Para Jeison/Fotoproducto/p3.png";
+    producto4.src = "img/Para Jeison/Fotoproducto/p4.png";
+    producto5.src = "img/Para Jeison/Fotoproducto/p5.png";
+    producto6.src = "img/Para Jeison/Fotoproducto/p6.png";
+    
+}
+
+mostrarProductos();
+
+let gastronomia1 = document.getElementById('gastronomia1');
+let gastronomia2 = document.getElementById('gastronomia2');
+let gastronomia3 = document.getElementById('gastronomia3');
+let gastronomia4 = document.getElementById('gastronomia4');
+let gastronomia5 = document.getElementById('gastronomia5');
+let gastronomia6 = document.getElementById('gastronomia6');
+let gastronomia7 = document.getElementById('gastronomia7');
+
+function mostrarGastronomia(){
+    gastronomia1.src = "img/Para Jeison/Gastronomía/g1.jpg";
+    gastronomia2.src = "img/Para Jeison/Gastronomía/g2.jpg";
+    gastronomia3.src = "img/Para Jeison/Gastronomía/g3.jpg";
+    gastronomia4.src = "img/Para Jeison/Gastronomía/g4.jpg";
+    gastronomia5.src = "img/Para Jeison/Gastronomía/g5.jpg";
+    gastronomia6.src = "img/Para Jeison/Gastronomía/g6.jpeg";
+    gastronomia7.src = "img/Para Jeison/Gastronomía/g7.jpeg";
+}
+mostrarGastronomia();
+
+let paisajes1 = document.getElementById('paisajes1');
+let paisajes2 = document.getElementById('paisajes2');
+let paisajes3 = document.getElementById('paisajes3');
+let paisajes4 = document.getElementById('paisajes4');
+let paisajes5 = document.getElementById('paisajes5');
+let paisajes6 = document.getElementById('paisajes6');
+let paisajes7 = document.getElementById('paisajes7');
+let paisajes8 = document.getElementById('paisajes8');
+let paisajes9 = document.getElementById('paisajes9');
+let paisajes10 = document.getElementById('paisajes10');
+let paisajes11 = document.getElementById('paisajes11');
+let paisajes12 = document.getElementById('paisajes12');
+let paisajes13 = document.getElementById('paisajes13');
+
+function mostrarPaisajes(){
+    paisajes1.src = "img/Para Jeison/Paisajes/p1.jpeg";
+    paisajes2.src = "img/Para Jeison/Paisajes/p2.jpeg";
+    paisajes3.src = "img/Para Jeison/Paisajes/p3.jpg";
+    paisajes4.src = "img/Para Jeison/Paisajes/p4.jpg";
+    paisajes5.src = "img/Para Jeison/Paisajes/p5.jpg";
+    paisajes6.src = "img/Para Jeison/Paisajes/p6.jpg";
+    paisajes7.src = "img/Para Jeison/Paisajes/p7.jpg";
+    paisajes8.src = "img/Para Jeison/Paisajes/p8.jpg";
+    paisajes9.src = "img/Para Jeison/Paisajes/p9.jpeg";
+    paisajes10.src = "img/Para Jeison/Paisajes/p10.jpeg";
+    paisajes11.src = "img/Para Jeison/Paisajes/p11.jpeg";
+    paisajes12.src = "img/Para Jeison/Paisajes/p12.jpeg";
+    paisajes13.src = "img/Para Jeison/Paisajes/p13.jpeg";
+    paisajes14.src = "img/Para Jeison/Paisajes/p14.jpeg";
+
+}
+
+mostrarPaisajes();
+
+let retratos1 = document.getElementById('retratos1');
+let retratos2 = document.getElementById('retratos2');
+let retratos3 = document.getElementById('retratos3');
+let retratos4 = document.getElementById('retratos4');
+let retratos5 = document.getElementById('retratos5');
+let retratos6 = document.getElementById('retratos6');
+let retratos7 = document.getElementById('retratos7');
+let retratos8 = document.getElementById('retratos8');
+let retratos9 = document.getElementById('retratos9');
+let retratos10 = document.getElementById('retratos10');
+let retratos11 = document.getElementById('retratos11');
+let retratos12 = document.getElementById('retratos12');
+let retratos13 = document.getElementById('retratos13');
+let retratos14 = document.getElementById('retratos14');
+let retratos15 = document.getElementById('retratos15');
+let retratos16 = document.getElementById('retratos16');
+let retratos17 = document.getElementById('retratos17');
+
+
+
+function mostrarRetratos(){
+        retratos1.src = "img/Para Jeison/Retratos/r1.jpeg";
+        retratos2.src = "img/Para Jeison/Retratos/r2.jpeg";
+        retratos3.src = "img/Para Jeison/Retratos/r3.jpeg";
+        retratos4.src = "img/Para Jeison/Retratos/r4.jpg";
+        retratos5.src = "img/Para Jeison/Retratos/r5.jpg";
+        retratos6.src = "img/Para Jeison/Retratos/r6.jpg";
+        retratos7.src = "img/Para Jeison/Retratos/r7.jpg";
+        retratos8.src = "img/Para Jeison/Retratos/r8.jpg";
+        retratos9.src = "img/Para Jeison/Retratos/r9.jpeg";
+        retratos10.src = "img/Para Jeison/Retratos/r10.jpeg";
+        retratos11.src = "img/Para Jeison/Retratos/r11.jpeg";
+        retratos12.src = "img/Para Jeison/Retratos/r12.jpeg";
+        retratos13.src = "img/Para Jeison/Retratos/r13.jpeg";
+        retratos14.src = "img/Para Jeison/Retratos/r14.jpeg";
+        retratos15.src = "img/Para Jeison/Retratos/r15.jpeg";
+        retratos16.src = "img/Para Jeison/Retratos/r16.jpeg";
+        retratos17.src = "img/Para Jeison/Retratos/r17.jpeg";
+        
+    }
+
+    mostrarRetratos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+  /*  const productos = [
+        
+    "img/Para Jeison/Fotoproducto/p2.png",
+    
+    "img/Para Jeison/Fotoproducto/p3.png",
+    
+    "img/Para Jeison/Fotoproducto/p4.png",
+    
+    "img/Para Jeison/Fotoproducto/p6.png"
+
+       
+    ];
+let contador = productos.length;
+
+function muestraProducto ()
+{
+    for(let i=0 ; i<productos.length ; i++){
+        if(i < producto.length){
+            producto =  productos[i];
+            contador =+ 1;
+        }if (contador == productos.length){
+            i= 0 ;
+        }
+      
+              
+    }
+    console.log(producto); 
+
+
+}
+
+muestraProducto();*/
+
